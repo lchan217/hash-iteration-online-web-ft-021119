c781427d7409d3e1162ab8f8b3225676f9487d9b
Lizzie Chan <lchan217@gmail.com>
2019-03-06 14:13:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 require "pry"
 def select_winner(passengers)
   passengers.each do |suite, name|
-    if suite == suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       name 
     end 
   end 
