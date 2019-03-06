38d6fb5d6ef367d2f8f9a23eaff53e00a75ce677
Lizzie Chan <lchan217@gmail.com>
2019-03-06 14:10:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age|
- puts "Happy Birthday #{kid}! You are now #{age} years old!"
- end
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end 
 end
 
 
